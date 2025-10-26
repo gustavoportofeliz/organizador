fbd96c28d575041962cccaf79ce220a95e86e700
Gustavo Santos Silvanio <gustavoportofeliz@gmail.com>
2025-10-26 01:22:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "organizador-de-clientes-knmjh",
+  "appId": "1:597370116180:web:ef43d892ec72946a1a35c1",
+  "apiKey": "AIzaSyAcJSOxyIFFIc5sISE7pjV0hxr2uwhLv2U",
+  "authDomain": "organizador-de-clientes-knmjh.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "597370116180"
+};
